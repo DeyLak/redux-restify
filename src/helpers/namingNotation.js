0a336e96e18269a4d101a4320f7883a4d5239d83
Nikolai Iakovlev <deylak@yandex.ru>
2018-06-15 14:45:22 +0300

    Fixed camelCase convertion

--- src/helpers/namingNotation.js
+++ src/helpers/namingNotation.js
@@ -7,7 +7,9 @@ const camelToSnake = s => s.replace(/([a-z])([A-Z]+)/g, '$1_$2')
 export const camelToLowerSnake = s => camelToSnake(s).toLowerCase()
 export const camelToUpperSnake = s => camelToSnake(s).toUpperCase()
 // Using camel to snake for exclude camelCase parts from converting to lower case
-export const snakeToCamel = s => camelToSnake(s).toLowerCase().replace(/_(.)/g, (match, group) => group.toUpperCase())
+export const snakeToCamel = s => {
+  return camelToSnake(s).toLowerCase().replace(/(.)_(.)/g, (match, g1, g2) => g1 + g2.toUpperCase())
+}
 
 const objectToCase = convertingFunc => (obj, config = {}) => {
   const {
