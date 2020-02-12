ba6373e01bc6f103b3fc1dabfbc4e9a43cf3bba4
Nikolai Iakovlev <deylak@yandex.ru>
2020-02-12 03:18:16 +0300

    Fixed nextPage for no pages bug 2

--- src/api/models/EntityList.js
+++ src/api/models/EntityList.js
@@ -565,9 +565,11 @@ class EntityList {
     const currentConfig = getPagesConfigHash(filter, sort, parentEntities, specialConfig, pageSizeNew, modelConfig)
     const currentPages = Object.keys(this.pages[currentConfig] || {})
     const currentCount = this.count[currentConfig] || 0
-    const lastPage = currentPages.map(x => +x).sort((a, b) => b - a)[0] || 0
+    const lastPage = currentPages.map(x => +x).sort((a, b) => b - a)[0]
 
-    return currentPages.length < currentCount / pageSizeNew ? lastPage + 1 : undefined
+    return (currentPages.length < currentCount / pageSizeNew) && lastPage
+      ? lastPage + 1
+      : undefined
   }
 
   getCount({
